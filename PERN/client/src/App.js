616aaf77e4d12fa73ed6f24a0035e5a2db9ec963
Syeda <syeda@almug.ai>
2020-05-27 12:15:05 +0530

    test

---
+++ PERN/client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import InputTodo from './Components/InputTodo';
+import ListTodo from './Components/ListTodo';
+
+function App() {
+  return (
+    <Fragment >
+      <div className="container">
+      <InputTodo />
+      <ListTodo/>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ PERN/client/src/Components/ListTodo.js
@@ -0,0 +1,65 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import EditTodo from './EditTodo'
+
+const ListTodo = () => {
+    const [todos, setTodos] = useState([]);
+//Delete
+const DeleteTodo = async id=>{
+    console.log(id)
+    try {
+        const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`,{
+            method: "DELETE"
+        });
+        //console.log(deleteTodo)
+
+    setTodos(todos.filter(todo => todo.todo_id !== id))
+        
+    } catch (err) {
+        console.error(err.message)
+        
+    }
+}
+   const getTodos = async() =>{
+
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json();
+            setTodos(jsonData)
+
+           // console.log(jsonData)
+            
+        } catch (err) {
+            console.error(err.message)
+            
+        }
+    };
+    useEffect(()=>{
+        getTodos();
+    },[])
+    console.log(todos);
+   
+    return (
+        <Fragment>
+            <table class="table text-center mt-5">
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo => (
+                        <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td><EditTodo todo={todo}/></td>
+                        <td><button className="btn btn-danger" onClick={()=>DeleteTodo(todo.todo_id)}>Delete</button></td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Fragment>
+    )
+
+}
+export default ListTodo;
